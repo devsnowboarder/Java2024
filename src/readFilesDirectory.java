3b636d6638ccade1e4c1f743ea78109e387f272e
Mike Lau <michael_snowboarder@yahoo.com>
2023-12-16 12:10:40 -0800

    add more files 12/16

---
+++ src/readFilesDirectory.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+
+public class readFilesDirectory {
+
+    public static void main(String[] args)
+    {
+        int count=0;
+        File folder = new File("/Volumes/mike_external_one/mike_web_albums/animalkingpark/images");
+
+        String[] files = folder.list();
+
+        for (String file : files)
+        {
+            System.out.println("picture.add("+"@"+file+"@"+");");
+            count++;
+
+        }
+
+        System.out.println(count);
+    }
+
+}
