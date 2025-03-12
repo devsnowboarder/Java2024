aa2871d6a89015ca967765bd6645c927bc78a878
Mike Lau <michael_snowboarder@yahoo.com>
2025-03-11 21:23:14 -0700

    new fork code

---
+++ src/This_Tutor/builderExample.java
@@ -0,0 +1,20 @@
+package This_Tutor;
+
+class Builder {
+    private String name;
+
+    public Builder setName(String name) {
+        this.name = name;
+        return this; // Returning current instance for method chaining
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+    }
+}
+
+public class builderExample {
+    public static void main(String[] args) {
+        new Builder().setName("John").display(); // Output: Name: John
+    }
+}
